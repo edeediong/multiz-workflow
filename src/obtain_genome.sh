6314818acf9d29e1724e437f9e539b833fc0b016
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-08-14 08:19:13 +0100

    directory issues

--- src/obtain_genome.sh
+++ src/obtain_genome.sh
@@ -1,10 +1,10 @@
-mkdir /data/genomes/dm6
+mkdir -p /data/genomes/dm6
 cd /data/genomes/dm6
 rsync -avzP \
     rsync://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.2bit .
 rsync -avzP \
     rsync://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.chrom.sizes .
-mkdir /data/genomes/dm6/trackData/GCF_000005575.2_AgamP3
+mkdir -p /data/genomes/dm6/trackData/GCF_000005575.2_AgamP3
 cd /data/genomes/dm6/trackData/GCF_000005575.2_AgamP3
 rsync -L -a -P rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/575/GCF_000005575.2_AgamP3/GCF_000005575.2_AgamP3_genomic.fna.gz ./
 rsync -L -a -P \
